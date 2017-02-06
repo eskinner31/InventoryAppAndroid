62baafe2d6f45a4b2dc1390cd3aa2b0caa8b0943
Eugene Skinner <eugene.skinner@homes.com>
2017-02-05 19:25:23 -0500

    implemented inventorydbhelper

--- app/src/main/java/com/example/android/inventoryapp/data/InventoryDbHelper.java
+++ app/src/main/java/com/example/android/inventoryapp/data/InventoryDbHelper.java
@@ -1,6 +1,9 @@
 package com.example.android.inventoryapp.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import com.example.android.inventoryapp.data.InventoryContract.InventoryEntry;
 
 /**
  * Created by Skinner on 2/5/17.
@@ -11,4 +14,27 @@ public class InventoryDbHelper extends SQLiteOpenHelper {
     public static final String TAG = InventoryDbHelper.class.getSimpleName();
 
     private static final String DATABASE_NAME = "inventory.db";
+
+    private static final int DATABASE_VERSION = 1;
+
+    public InventoryDbHelper(Context context) { super(context, DATABASE_NAME, null, DATABASE_VERSION); }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String SQL_CREATE_INVENTORY_TABLE = "CREATE TABLE " + InventoryEntry.TABLE_NAME + " ("
+                + InventoryEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + InventoryEntry.COLUMN_ITEM_NAME + "TEXT NOT NULL, "
+                + InventoryEntry.COLUMN_SUPPLIER_NAME + "TEXT NOT NULL, "
+                + InventoryEntry.COLUMN_SUPPLIER_EMAIL + "TEXT NOT NULL, "
+                + InventoryEntry.COLUMN_STOCK + "INTEGER NOT NULL, "
+                + InventoryEntry.COLUMN_PRICE + "INTEGER NOT NULL, "
+                + InventoryEntry.COLUMN_IMAGE + "BLOB NOT NULL);";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_INVENTORY_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        //Research strategies for updating the DB
+    }
 }
